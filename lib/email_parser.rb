35cf13dfd573c7000f7104a497ab5f3e9dd043a0
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-15 08:36:32 -0500

    commit tree 24ac2fd789808a3de872be74d1cb7499c8c76d4b

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,18 @@
 # Build a class EmailAddressParser that accepts a string of unformatted 
+
+class EmailAddressParser
+    attr_accessor :new_email
+
+    def initialize(new_email)
+        @new_email = new_email
+    end
+
+    
+end
+
+email_addresses = "1@gmail.com, 2@gmail.com, 3@gmail.com"
+parser = EmailAddressParser.new(email_addresses)
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
